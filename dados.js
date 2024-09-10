e87559cc5dae95f9070afadde91e33daea39879b
michaeldowding-homer <michael.abrum.dowding@gmail.com>
2024-09-10 20:55:37 -0300

    Ajuste no campo de pesquisa

--- dados.js
+++ dados.js
@@ -5,65 +5,76 @@ let dados = [
         titulo: "Homer Simpson",
         descricao: "Homer Jay Simpson é o pai da família Simpson, conhecido por sua personalidade excêntrica, amor por donuts, cerveja Duff  e sua constante busca por momentos de lazer. Trabalhando na usina nuclear de Springfield, Homer equilibra (ou tenta equilibrar) sua vida familiar com sua paixão por televisão, boliche e, claro, a comida.",
         link: "https://simpsons.fandom.com/pt/wiki/Homer_Simpson"
+        //tags: "simpsons, pai, cerveja, donut, usina nuclear"
     },
     // Marge Simpson
     {
         titulo: "Marge Simpson",
         descricao: "Marjorie Jacqueline \"Marge\" Bouvier Simpson é a matriarca da família Simpson, conhecida por seu cabelo azul e sua personalidade paciente e gentil. Ela é casada com Homer e mãe de Bart, Lisa e Maggie.",
         link: "https://simpsons.fandom.com/pt/wiki/Marge_Simpson"
+        //tags: ["simpsons", "mãe", "cabelo azul", "paciente", "gentil"]
       },
     // Bart Simpson
     {
         titulo: "Bart Simpson",
         descricao: "Bart Simpson é o filho mais velho da família Simpson, conhecido por suas travessuras e seu sarcasmo. Ele é um estudante da Escola Elementar de Springfield e um dos personagens mais populares da série.",
         link: "https://simpsons.fandom.com/pt/wiki/Bart_Simpson"
+        //tags: ["filho, travesso, skate, Krusty, escola, Springfield"]
     },
     // Lisa Simpson
     {
         titulo: "Lisa Simpson",
         descricao: "Lisa Marie Simpson é a filha do meio da família Simpson, uma garota inteligente, talentosa e musical. Ela toca saxofone, é vegetariana e defensora dos direitos dos animais. Lisa é frequentemente vista lendo livros e tentando fazer a diferença no mundo.",
         link: "https://simpsons.fandom.com/pt/wiki/Lisa_Simpson"
+        //tags: ["inteligente, música, saxofone, vegetariana, ativista, escola"]
     },
     // Maggie Simpson
     {   
         titulo: "Maggie Simpson",
         descricao: "Margaret Evelyn 'Maggie' Simpson é a mais nova membro da família Simpson, uma bebê que quase nunca fala. Ela é conhecida por sua chupeta e seu rifle de brinquedo.",
         link: "https://simpsons.fandom.com/pt/wiki/Maggie_Simpson"
+        //tags: ["bebê, chupeta, rifle, inocente"]
     },
     // Ned Flanders
     {
         titulo: "Ned Flanders",
         descricao: "Ned Flanders é o vizinho religioso e extremamente amigável, gosta de ajudar a todos possui uma esposa e 2 filhos. Muito preocupado com seus filhos, Ned impede que eles assistam certos programas da televisão e comam doces com açúcar.",
         link: "https://simpsons.fandom.com/pt/wiki/Ned_Flanders"
+        //tags: ["religioso, vizinho, amigável, igreja, família"]
     },
     // Moe Szyslak
     {
         titulo: "Morris Lester Moe",
         descricao: "Morris Lester 'Moe' é o proprietário e barman da Taverna de Moe, um bar que o Homer ama frequentar, ele é amargo e quase sempre desprezado por todos incluindo seus melhores amigos.",
         link: "https://simpsons.fandom.com/pt/wiki/Moe_Szyslak"
+        //tags: ["barman, taverna, solitário, amargo"]
     },
     // Charles Montgomery Burns
     {
         titulo: "Charles Montgomery Burns",
         descricao: "Charles Montgomery Burns é o clássico vilão rico e poderoso, conhecido por sua ganância insaciável e falta de escrúpulos. Sua idade avançada o tornou fisicamente frágil, com diversas doenças e limitações. No entanto, sua mente continua afiada e manipuladora, o que o torna ainda mais perigoso.",
         link: "https://simpsons.fandom.com/pt/wiki/Charles_Montgomery_Burns"
+        //tags: ["rico, velho, ganancioso, usina nuclear"]
     },
     // Sideshow Bob
     {
         titulo: "Robert Sideshow Bob",
         descricao: "Sideshow Bob é um personagem icônico dos Simpsons, conhecido por sua obsessão em prejudicar Bart Simpson. Com uma inteligência acima da média e um ego inflado, Bob trama planos elaborados para se vingar de Bart, que sempre consegue frustrar seus intentos.",
         link: "https://simpsons.fandom.com/pt/wiki/Robert_Terwilliger"
+        //tags: ["vilão, inteligente, vingança, Bart"]
     },
     // Apu Nahasapeemapetilon
     {
         titulo: "Dr. Apu Nahasapeemapetilon, Ph.D. Jr",
         descricao: "Apu Nahasapeemapetilon é um personagem do desenho animado Os Simpsons. Baseado no esterótipo dos asiáticos donos de pequenos mercados nos Estados Unidos da América, EUA, é dublado originalmente por Hank Azaria. Em um episódio ele diz que é vegetariano.",
         link: "https://simpsons.fandom.com/pt/wiki/Apu_Nahasapeemapetilon"
+        //tags: ["indiano, Kwik-E-Mart, família grande, nerd"]
     },
     // Krusty, o Palhaço
     {
         titulo: "Herschel Shmoikel Pinkus Yerucham Krustofski",
         descricao: "Krusty é mostrado na série como um veterano do entretenimento cansado de sua fama e de lidar com crianças. Devido a sua infelicidade tornou-se viciado em jogo, cigarros, álcool, Percodan, Pepto-Bismol e Xanax.",
         link: "https://simpsons.fandom.com/pt/wiki/Krusty,_o_Palha%C3%A7o"
+        //tags: ["palhaço, programa de TV, vício, azar"]
     },
 ];
